54d501174b80c150eb64f099a310b651dbdfaf4a
Vladislav Kostikov <vlad@kostikov.ru>
2023-11-13 20:05:28 +0300

    Оптимизировал рекурсивную проверку на палиндром

--- 1.3.2-8_tasks/palindrome.py
+++ 1.3.2-8_tasks/palindrome.py
@@ -1,6 +1,10 @@
-def palindrome(string: str) -> bool:
-    if len(string) <= 1:
+def palindrome(string: str, step: int = 0) -> bool:
+    if step >= len(string) // 2:
         return True
-    if string[0] != string[-1]:
+
+    first_char_for_step = string[step]
+    last_char_for_step = string[-1 - step]
+    if first_char_for_step != last_char_for_step:
         return False
-    return palindrome(string[1:-1])
+
+    return palindrome(string, step + 1)
