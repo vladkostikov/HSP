65157c50eb9e9f1ad675f14120940b26ff282aca
Vladislav Kostikov <vlad@kostikov.ru>
2023-11-15 16:53:01 +0300

    Убрал if при проверке чётности

--- 1.3.2-8_tasks/printing_values_with_even_indexes.py
+++ 1.3.2-8_tasks/printing_values_with_even_indexes.py
@@ -1,8 +1,7 @@
-def printing_values_with_even_indexes(array: list, index: int = 0):
+def printing_values_with_even_indexes(array: list, index: int = 0, printing: bool = True):
     if len(array) <= index:
         return
 
-    if index % 2 == 0:
-        print(array[index])
+    printing and print(array[index])
 
-    printing_values_with_even_indexes(array, index + 1)
+    printing_values_with_even_indexes(array, index + 1, not printing)
