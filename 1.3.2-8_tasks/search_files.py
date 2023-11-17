a67c9f97c7ad0336d24fceaa9b7c7b8eb42b9353
Vladislav Kostikov <vlad@kostikov.ru>
2023-11-17 08:43:30 +0300

    Сделал поиск всех файлов в директории рекурсией

---
+++ 1.3.2-8_tasks/search_files.py
@@ -0,0 +1,18 @@
+import os
+
+
+def search_files(path: str = ".") -> list:
+    current_directory = os.listdir(path)
+    files_in_current_directory = current_directory
+
+    for file in current_directory:
+        path_to_file = path + "/" + file
+        is_directory = os.path.isdir(path_to_file)
+
+        files_in_directory = []
+        if is_directory:
+            files_in_directory = search_files(path_to_file)
+
+        files_in_current_directory.extend(files_in_directory)
+
+    return files_in_current_directory
