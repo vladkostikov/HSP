3d9c3d02a47bac2d47a81ea3ed51dc377c48e910
Vladislav Kostikov <vlad@kostikov.ru>
2023-11-18 15:37:34 +0300

    Рефакторинг поиска второго максимума рекурсией

--- 1.3.2-8_tasks/find_second_maximum_number.py
+++ 1.3.2-8_tasks/find_second_maximum_number.py
@@ -1,15 +1,17 @@
-def find_second_maximum_number(array: list, maximum_indexes: list = None) -> int:
+def find_second_maximum_number(numbers: list) -> int:
+    second_maximum_index = find_second_maximum_number_index(numbers, [])
+    return numbers[second_maximum_index]
+
+
+def find_second_maximum_number_index(numbers: list, maximum_indexes: list) -> int:
     maximum_index = 0
-    if maximum_indexes is None:
-        maximum_indexes = []
-    if 0 in maximum_indexes:
+    if maximum_index in maximum_indexes:
         maximum_index = 1
 
-    for index, num in enumerate(array):
-        if (num > array[maximum_index]) and (index not in maximum_indexes):
+    for index, num in enumerate(numbers):
+        if (num > numbers[maximum_index]) and (index not in maximum_indexes):
             maximum_index = index
 
     if len(maximum_indexes) == 0:
-        return find_second_maximum_number(array, maximum_indexes + [maximum_index])
-
-    return array[maximum_index]
+        return find_second_maximum_number_index(numbers, [maximum_index])
+    return maximum_index
