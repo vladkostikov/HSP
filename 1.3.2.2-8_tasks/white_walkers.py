421d886b83e65a27059972a3cb440be13fb0644a
Vladislav Kostikov <vlad@kostikov.ru>
2023-11-22 23:18:20 +0300

    Переделал решение по более правильному примеру

--- 1.3.2.2-8_tasks/white_walkers.py
+++ 1.3.2.2-8_tasks/white_walkers.py
@@ -3,22 +3,21 @@ def white_walkers(village: str) -> bool:
     for num in range(0, 10):
         digits.append(str(num))
 
-    second_cityzen_index = None
+    first_cityzen = 0
+    white_walkers_between_cityzens = 0
     is_walkers_identified = False
-    for i, char in enumerate(village):
-        if char not in digits:
-            continue
+    for char in village:
+        if char in digits:
+            second_cityzen = char
+            sum_of_two_citizens = int(first_cityzen) + int(second_cityzen)
 
-        if second_cityzen_index is None:
-            second_cityzen_index = i
-            continue
+            if sum_of_two_citizens == 10:
+                if white_walkers_between_cityzens != 3:
+                    return False
+                is_walkers_identified = True
+            first_cityzen = second_cityzen
+            white_walkers_between_cityzens = 0
+        elif char == "=":
+            white_walkers_between_cityzens += 1
 
-        first_cityzen_index = second_cityzen_index
-        second_cityzen_index = i
-        sum_of_two_citizens = int(village[first_cityzen_index]) + int(village[second_cityzen_index])
-        if sum_of_two_citizens == 10:
-            white_walkers_between_cityzens = village[first_cityzen_index + 1:second_cityzen_index].count("=")
-            if white_walkers_between_cityzens != 3:
-                return False
-            is_walkers_identified = True
     return is_walkers_identified
