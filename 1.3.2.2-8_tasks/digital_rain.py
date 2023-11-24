5d8c4f229a3b0d94c73abc59585526caf4044999
Vladislav Kostikov <vlad@kostikov.ru>
2023-11-24 23:58:55 +0300

    Переделал решение по поиску подстроки по устному объяснению

--- 1.3.2.2-8_tasks/digital_rain.py
+++ 1.3.2.2-8_tasks/digital_rain.py
@@ -1,38 +1,33 @@
 def digital_rain(col: str) -> str:
-    counter0 = 0
-    counter1 = 0
-    for char in col:
-        if char == "0":
-            counter0 += 1
-        elif char == "1":
-            counter1 += 1
-
+    counter0 = {}
+    counter1 = {}
+    differences_by_index = {}
+    differences_by_value = {}
+    max_length = 0
     start_index = 0
-    end_index = len(col) - 1
-    for _char in col:
-        if counter0 == counter1:
-            return col[start_index:end_index + 1]
+    end_index = 0
+    for i, char in enumerate(list(col)):
+        if char == "0":
+            counter0[i] = counter0.get(i - 1, 0) + 1
+            counter1[i] = counter1.get(i - 1, 0)
+        elif char == "1":
+            counter1[i] = counter1.get(i - 1, 0) + 1
+            counter0[i] = counter0.get(i - 1, 0)
+        differences_by_index[i] = counter0[i] - counter1[i]
 
-        if counter0 < counter1:
-            if col[start_index] == "1":
-                start_index += 1
-                counter1 -= 1
-            elif col[end_index] == "1":
-                end_index -= 1
-                counter1 -= 1
-            else:
-                start_index += 1
-                counter0 -= 1
+        if differences_by_index[i] == 0:
+            start_index = 0
+            end_index = i + 1
+            max_length = end_index - start_index
+        elif differences_by_value.get(differences_by_index[i]) is not None:
+            new_start_index = differences_by_value[differences_by_index[i]] + 1
+            new_end_index = i + 1
+            new_max_length = new_end_index - new_start_index
+            if new_max_length >= max_length:
+                start_index = new_start_index
+                end_index = new_end_index
+                max_length = new_max_length
+        else:
+            differences_by_value[differences_by_index[i]] = i
 
-        elif counter1 < counter0:
-            if col[start_index] == "0":
-                start_index += 1
-                counter0 -= 1
-            elif col[end_index] == "0":
-                end_index -= 1
-                counter0 -= 1
-            else:
-                start_index += 1
-                counter1 -= 1
-
-    return col[start_index:end_index + 1]
+    return col[start_index:end_index]
--- 1.3.2.2-8_tasks/test_digital_rain.py
+++ 1.3.2.2-8_tasks/test_digital_rain.py
@@ -4,8 +4,12 @@ from digital_rain import digital_rain
 
 class Test(TestCase):
     def test_digital_rain(self):
+        self.assertEqual("110110010010", digital_rain("00001101100100100000"))
         self.assertEqual("111000", digital_rain("1111000"))
         self.assertEqual("11101000", digital_rain("11101000"))
         self.assertEqual("10", digital_rain("011111110"))
         self.assertEqual("", digital_rain("11111111"))
         self.assertEqual("1100", digital_rain("000011000"))
+        self.assertEqual("110110010010", digital_rain("00001101100100100000"))
+        self.assertEqual("10", digital_rain("00100010"))
+        self.assertEqual("1100", digital_rain("11000001100"))
