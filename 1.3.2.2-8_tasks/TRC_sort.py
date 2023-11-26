c95ba9a79376ea40e816c5fe7a1b606880fc2a95
Vladislav Kostikov <vlad@kostikov.ru>
2023-11-26 15:10:27 +0300

    Переделал в рекурсию сортировку массива из 0, 1, 2

--- 1.3.2.2-8_tasks/TRC_sort.py
+++ 1.3.2.2-8_tasks/TRC_sort.py
@@ -1,17 +1,19 @@
 def TRC_sort(trc: list) -> list:
-    left0 = -1
-    right1 = 0
-    left2 = len(trc) - 1
+    return trc_sort(trc, -1, 0, len(trc) - 1)
 
-    while right1 <= left2:
-        if trc[right1] == 0:
-            trc[right1], trc[left0 + 1] = trc[left0 + 1], trc[right1]
-            left0 += 1
-            right1 += 1
-        elif trc[right1] == 1:
-            right1 += 1
-        elif trc[right1] == 2:
-            trc[right1], trc[left2] = trc[left2], trc[right1]
-            left2 -= 1
 
-    return trc
+def trc_sort(trc: list, left0: int, right1: int, left2: int) -> list:
+    if right1 > left2:
+        return trc
+
+    if trc[right1] == 0:
+        trc[right1], trc[left0 + 1] = trc[left0 + 1], trc[right1]
+        left0 += 1
+        right1 += 1
+    elif trc[right1] == 1:
+        right1 += 1
+    elif trc[right1] == 2:
+        trc[right1], trc[left2] = trc[left2], trc[right1]
+        left2 -= 1
+
+    return trc_sort(trc, left0, right1, left2)
--- 1.3.2.2-8_tasks/test_TRC_sort.py
+++ 1.3.2.2-8_tasks/test_TRC_sort.py
@@ -5,5 +5,4 @@ from TRC_sort import TRC_sort
 class Test(TestCase):
     def test_trc_sort(self):
         self.assertEqual([0, 1, 2], TRC_sort([2, 1, 0]))
-        self.assertEqual([0, 1, 2], TRC_sort([2, 1, 0]))
         self.assertEqual([0, 0, 1, 1, 2, 2], TRC_sort([0, 1, 2, 1, 0, 2]))
