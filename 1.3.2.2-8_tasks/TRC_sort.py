3084132b281cd46d9827df91688ce49c9294218c
Vladislav Kostikov <vlad@kostikov.ru>
2023-11-26 15:15:38 +0300

    Итоговая версия сортировки массива из 0, 1, 2 с использованием хеша

--- 1.3.2.2-8_tasks/TRC_sort.py
+++ 1.3.2.2-8_tasks/TRC_sort.py
@@ -1,19 +1,7 @@
 def TRC_sort(trc: list) -> list:
-    return trc_sort(trc, -1, 0, len(trc) - 1)
+    numbers = {}
 
+    for num in trc:
+        numbers[num] = numbers.get(num, 0) + 1
 
-def trc_sort(trc: list, left0: int, right1: int, left2: int) -> list:
-    if right1 > left2:
-        return trc
-
-    if trc[right1] == 0:
-        trc[right1], trc[left0 + 1] = trc[left0 + 1], trc[right1]
-        left0 += 1
-        right1 += 1
-    elif trc[right1] == 1:
-        right1 += 1
-    elif trc[right1] == 2:
-        trc[right1], trc[left2] = trc[left2], trc[right1]
-        left2 -= 1
-
-    return trc_sort(trc, left0, right1, left2)
+    return [0] * numbers[0] + [1] * numbers[1] + [2] * numbers[2]
